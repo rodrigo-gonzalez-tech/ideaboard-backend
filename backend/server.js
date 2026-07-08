3a2e1b6fcfe5dfa6a9c3175ba5a8bffc3dae06ab
Rodri <rodrigo.gonzalez.tech@gmail.com>
2026-07-08 17:32:48 +0200

    Enable CORS

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,17 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db.js");
+const cors = require("cors");
+const ideasRouter = require("./routes/ideas.js");
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Body parser middleware
 app.use(express.json());
@@ -19,7 +24,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Idea Board API." });
 });
 
-const ideasRouter = require("./routes/ideas.js");
 app.use("/api/ideas", ideasRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}.`));
